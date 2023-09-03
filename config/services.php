df96fe96b53c9459536a64525f45e0180fb7b625
duong1801 <duongx1801@gmail.com>
2023-09-03 23:35:26 +0700

    update code

--- config/services.php
+++ config/services.php
@@ -32,8 +32,8 @@ return [
     ],
 
     'facebook' => [
-        'client_id' => '674128557951349',
-        'client_secret' => '168b978ece81c4348bf641de747efa60',
+        'client_id' => '1246426352689214',
+        'client_secret' => 'c87262928e5c695fd37e5d8a123416e0',
         'redirect' => 'https://bluesky.io.vn/callback',
     ],
 
